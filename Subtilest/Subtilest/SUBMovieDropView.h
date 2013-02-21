be2223edabbfef9d6518256f7b8bad21221b1ac2
Romain Ruetschi <romain.ruetschi@gmail.com>
2013-02-21 17:04:48 +0100

    Add an empty drop view subclass.

---
+++ Subtilest/Subtilest/SUBMovieDropView.h
@@ -0,0 +1,13 @@
+//
+//  SUBMovieDropView.h
+//  Subtilest
+//
+//  Created by Romain Ruetschi on 21.02.13.
+//  Copyright (c) 2013 Frederic Jacobs. All rights reserved.
+//
+
+#import "SUBDropView.h"
+
+@interface SUBMovieDropView : SUBDropView
+
+@end
---
+++ Subtilest/Subtilest/SUBMovieDropView.m
@@ -0,0 +1,28 @@
+//
+//  SUBMovieDropView.m
+//  Subtilest
+//
+//  Created by Romain Ruetschi on 21.02.13.
+//  Copyright (c) 2013 Frederic Jacobs. All rights reserved.
+//
+
+#import "SUBMovieDropView.h"
+
+@implementation SUBMovieDropView
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    // Drawing code here.
+}
+
+@end
